4d2f5bcd0e02b2cb263e1b227fdf7f9c3a727be6
Thong <anhthongvu1996@gmail.com>
2024-12-14 16:09:30 +0700

    multi grep telescope

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -46,5 +46,8 @@ return {
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+
+    -- multil grep
+    require("config.plugins.telescope.telescope-multil-grep").setup()
   end,
 }
