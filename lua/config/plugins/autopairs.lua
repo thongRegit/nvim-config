d84e311c0e5d0aa36c7f6250598ca4f04d70d6b0
Thong <anhthongvu1996@gmail.com>
2024-12-18 23:57:40 +0700

    fix: change config autopair

--- lua/config/plugins/autopairs.lua
+++ lua/config/plugins/autopairs.lua
@@ -2,7 +2,7 @@ return {
   "windwp/nvim-autopairs",
   event = { "InsertEnter" },
   dependencies = {
-    "saghen/blink.cmp",
+    "saghen/blink.cmp", -- Đảm bảo plugin blink.cmp được tải trước
   },
   config = function()
     -- import nvim-autopairs
@@ -13,7 +13,7 @@ return {
       check_ts = true, -- enable treesitter
       ts_config = {
         lua = { "string" }, -- don't add pairs in lua string treesitter nodes
-        javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
+        javascript = { "template_string" }, -- don't add pairs in javascript template_string treesitter nodes
         java = false, -- don't check treesitter on java
       },
     })
@@ -21,10 +21,10 @@ return {
     -- import nvim-autopairs completion functionality
     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
-    -- import nvim-cmp plugin (completions plugin)
-    local cmp = require("cmp")
+    -- import blink.cmp plugin
+    local blink_cmp = require("blink.cmp")
 
-    -- make autopairs and completion work together
-    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    -- make autopairs and blink.cmp work together
+    blink_cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
   end,
 }
