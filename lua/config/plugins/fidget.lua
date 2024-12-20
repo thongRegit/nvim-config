dcef9878c32a5136c2073c3e553f345633a930b4
Thong <anhthongvu1996@gmail.com>
2024-12-20 23:44:40 +0700

    fix: remove error config in blink-cmp and fidget

--- lua/config/plugins/fidget.lua
+++ lua/config/plugins/fidget.lua
@@ -6,15 +6,6 @@ return {
         winblend = 0,
         border = "none",
       },
-      text = {
-        spinner = "dots", -- Kiểu spinner (dạng chấm động)
-        done = "✔", -- Ký hiệu hiển thị khi hoàn thành
-      },
-    },
-    timer = {
-      spinner_rate = 125, -- Tốc độ chuyển động của spinner (ms) ]]
-      fidget_decay = 2000, -- Thời gian hiển thị spinner sau khi task hoàn thành (ms) ]]
-      task_decay = 1000, -- Thời gian task được giữ lại sau khi hoàn thành (ms) ]]
     },
   },
 }
